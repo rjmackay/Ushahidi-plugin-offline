89c27b5093f3441a69ebcc51221f7c1e4b4f89f7
Robbie Mackay <rm@robbiemackay.com>
2012-03-16 18:05:58 +1300

    Reformat offline.php

--- controllers/offline.php
+++ controllers/offline.php
@@ -3,13 +3,13 @@
  * Offline Controller
  *
  * PHP version 5
- * LICENSE: This source file is subject to LGPL license 
+ * LICENSE: This source file is subject to LGPL license
  * that is available through the world-wide-web at the following URI:
  * http://www.gnu.org/copyleft/lesser.html
- * @author     Ushahidi Team <team@ushahidi.com> 
+ * @author     Ushahidi Team <team@ushahidi.com>
  * @package    Ushahidi - http://source.ushahididev.com
  * @copyright  Ushahidi - http://www.ushahidi.com
- * @license    http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL) 
+ * @license    http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
  */
 
 class Offline_Controller extends Main_Controller
@@ -63,52 +63,52 @@ class Offline_Controller extends Main_Controller
 		if (!$rev)
 		{
 			$rev = time();
-			$this->cache->set('offline_appcache_rev',$rev);
+			$this->cache->set('offline_appcache_rev', $rev);
 		}
-		
+
 		$content .= "# Rev: $rev\n";
 		$content .= "CACHE\n";
-		
+
 		$content .= "/offline\n";
 		//$content .= "/offline/\n";
 		$content .= "/media/js/jquery.js\n";
-		
-		$plugindir = PLUGINPATH.'offline';
-		
-		if ( is_dir($plugindir.'/css') )
+
+		$plugindir = PLUGINPATH . 'offline';
+
+		if (is_dir($plugindir . '/css'))
 		{
-			$dir = dir($plugindir.'/css');
+			$dir = dir($plugindir . '/css');
 			while (($file = $dir->read()) !== FALSE)
 				if (preg_match("/\.css/i", $file))
 				{
-					$content .= "/plugins/offline/css/".$file."\n";
+					$content .= "/plugins/offline/css/" . $file . "\n";
 				}
 		}
-		
-		if ( is_dir($plugindir.'/js') )
+
+		if (is_dir($plugindir . '/js'))
 		{
-			$dir = dir($plugindir.'/js');
+			$dir = dir($plugindir . '/js');
 			while (($file = $dir->read()) !== FALSE)
 				if (preg_match("/\.js/i", $file))
 				{
-					$content .= "/plugins/offline/js/".$file."\n";
+					$content .= "/plugins/offline/js/" . $file . "\n";
 				}
 		}
-		
-		if ( is_dir($plugindir.'/css') )
+
+		if (is_dir($plugindir . '/css'))
 		{
-			$dir = dir($plugindir.'/images');
+			$dir = dir($plugindir . '/images');
 			while (($file = $dir->read()) !== FALSE)
 				if ($file != '.' AND $file != '..')
-					$content .= "/plugins/offline/images/".$file."\n";
+					$content .= "/plugins/offline/images/" . $file . "\n";
 		}
 		$content .= "NETWORK\n";
 		$content .= "/api\n";
 		$content .= "*\n";
-		
+
 		echo $content;
-		
+
 		exit();
 	}
-	
+
 }
