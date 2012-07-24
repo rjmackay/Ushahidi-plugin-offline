95fb133b45bbf108b168012ea826f88c55f17d25
Robbie Mackay <rm@robbiemackay.com>
2012-07-24 15:29:13 +1200

    Clean formatting on ushahidi.sync.js and remove unused code

--- js/ushahidi.sync.js
+++ js/ushahidi.sync.js
@@ -1,6 +1,6 @@
 /*
-* Custom Ushahidi Backbone.sync tweaks
-*/
+ * Custom Ushahidi Backbone.sync tweaks
+ */
 
 /* Create basic auth token */
 function make_base_auth(user, password) {
@@ -18,65 +18,53 @@ Offline.syncWithAuth = function(method, model, options, error) {
 	// Add username / password
 	if (!options.username)
 	{
-		options.beforeSend = function(xhr, jqsettings)
-		{
+		options.beforeSend = function(xhr, jqsettings) {
 			xhr.setRequestHeader('Authorization', make_base_auth(settings.get('username'), settings.get('password')));
 		}
 	}
-	
+
 	Backbone.ajaxSync.apply(this, [method, model, options, error]);
 };
 
 // Override offline sync to use syncwithAuth
 Offline.sync = function(method, model, options) {
 	var store, _ref;
-	store = model.storage || ((_ref = model.collection) != null ? _ref.storage : void 0);
-	if (store && (store != null ? store.support : void 0)) {
+	store = model.storage || (( _ref = model.collection) != null ? _ref.storage :
+	void 0);
+	if (store && (store != null ? store.support :
+	void 0))
+	{
 		return Offline.localSync(method, model, options, store);
-	} else {
+	}
+	else
+	{
 		return Offline.syncWithAuth(method, model, options);
 	}
 };
 
 // Tweak the Backbone.offline ajax sync
 Offline.Sync.prototype.ajax = function(method, model, options) {
-	if (Offline.onLine()) {
+	if (Offline.onLine())
+	{
 		this.prepareOptions(options);
 
 		var settings = model.settings || model.collection.settings || window.app.model.settings;
-		
+
 		// add username / password
 		if (!options.username)
 		{
-			options.beforeSend = function(xhr, jqsettings)
-			{
+			options.beforeSend = function(xhr, jqsettings) {
 				xhr.setRequestHeader('Authorization', make_base_auth(settings.get('username'), settings.get('password')));
 			}
 		}
-		
+
 		return Backbone.ajaxSync(method, model, options);
-	} else {
+	}
+	else
+	{
 		return this.storage.setItem('offline', 'true');
 	}
 };
-
-// From app - could be used for custom online/offline behaviour
-/*
-reconnect : function() {
-	this.model.online = true;
-	this.model.startPolling(50);
-	$('#offline').hide();
-	return false;
-},
-fetchError : function(jqXHR, textStatus, errorThrown) {
-	if (textStatus.statusText == 'timeout')
-	{
-		this.stopPolling();
-		this.online = false;
-		$('#offline').show();
-	}
-}
-*/
 
 // Make sure we've got the right backbone sync.
 Backbone.sync = Offline.sync;
