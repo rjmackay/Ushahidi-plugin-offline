62ddfa4888a4c6c841613904a4705ebc5bce9877
Robbie Mackay <rm@robbiemackay.com>
2012-08-06 17:32:52 +1200

    Handle messages with reports not in local storage

--- i18n/en_US/offline.php
+++ i18n/en_US/offline.php
@@ -4,4 +4,5 @@ $lang = array
 (
 	'offline_version' => 'Offline Version',
 	'switch_to_offline_version' => 'Switch to Offline Version',
+	'report_not_available' => 'Report not available offline'
 );
--- views/offline/message-li-template.php
+++ views/offline/message-li-template.php
@@ -23,7 +23,11 @@
 						<% if (incident_id == 0) { %>
 							<li class="none-separator"><a href="#reports/add/from_message/<%- id %>"><?php echo Kohana::lang('ui_admin.create_report'); ?></a></li>
 						<% } else { %>
-							<li class="none-separator"><a href="#reports/view/<%- report.id %>"><?php echo Kohana::lang('ui_admin.view_report'); ?></a></li>
+							<% if (report != undefined) { %>
+								<li class="none-separator"><a href="#reports/view/<%- report.id %>" class="status_yes"><?php echo Kohana::lang('ui_admin.view_report'); ?></a></li>
+							<% } else { %>
+								<li class="none-separator"><?php echo Kohana::lang('offline.report_not_available'); ?></li>
+							<% } %>
 						<% } %>
 						<li><a href="#messages/delete/<%- id %>" class="del"><?php echo Kohana::lang('ui_main.delete'); ?></a></li>
 						<?php } ?>
